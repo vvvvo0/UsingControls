db9c9c63fdba1ad82b2bad22a76c9980bc57dd1d
vvvvo0 <idvvvvo@gmail.com>
2025-01-04 15:48:55 +0900

    컨트롤 배치: ComboBox, CheckBox, TextBox

--- MainForm.Designer.cs
+++ MainForm.Designer.cs
@@ -30,11 +30,19 @@
         {
             grpFont = new GroupBox();
             IblFont = new Label();
+            cboFont = new ComboBox();
+            chkBold = new CheckBox();
+            chkIltalic = new CheckBox();
+            txtSampleText = new TextBox();
             grpFont.SuspendLayout();
             SuspendLayout();
             // 
             // grpFont
             // 
+            grpFont.Controls.Add(txtSampleText);
+            grpFont.Controls.Add(chkIltalic);
+            grpFont.Controls.Add(chkBold);
+            grpFont.Controls.Add(cboFont);
             grpFont.Controls.Add(IblFont);
             grpFont.Location = new Point(21, 35);
             grpFont.Name = "grpFont";
@@ -47,18 +55,55 @@
             // IblFont
             // 
             IblFont.AutoSize = true;
-            IblFont.Location = new Point(28, 43);
+            IblFont.Location = new Point(43, 46);
             IblFont.Name = "IblFont";
-            IblFont.Size = new Size(31, 15);
+            IblFont.Size = new Size(38, 15);
             IblFont.TabIndex = 0;
-            IblFont.Text = "Font";
+            IblFont.Text = "Font :";
+            // 
+            // cboFont
+            // 
+            cboFont.FormattingEnabled = true;
+            cboFont.Location = new Point(89, 43);
+            cboFont.Name = "cboFont";
+            cboFont.Size = new Size(198, 23);
+            cboFont.TabIndex = 1;
+            // 
+            // chkBold
+            // 
+            chkBold.AutoSize = true;
+            chkBold.Location = new Point(302, 45);
+            chkBold.Name = "chkBold";
+            chkBold.Size = new Size(50, 19);
+            chkBold.TabIndex = 2;
+            chkBold.Text = "굵게";
+            chkBold.UseVisualStyleBackColor = true;
+            // 
+            // chkIltalic
+            // 
+            chkIltalic.AutoSize = true;
+            chkIltalic.Location = new Point(358, 45);
+            chkIltalic.Name = "chkIltalic";
+            chkIltalic.Size = new Size(62, 19);
+            chkIltalic.TabIndex = 3;
+            chkIltalic.Text = "이탤릭";
+            chkIltalic.UseVisualStyleBackColor = true;
+            // 
+            // txtSampleText
+            // 
+            txtSampleText.Location = new Point(44, 87);
+            txtSampleText.Name = "txtSampleText";
+            txtSampleText.Size = new Size(375, 23);
+            txtSampleText.TabIndex = 4;
+            txtSampleText.Text = "Hello, C#";
             // 
             // MainForm
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(800, 450);
+            ClientSize = new Size(621, 450);
             Controls.Add(grpFont);
+            FormBorderStyle = FormBorderStyle.FixedSingle;
             Name = "MainForm";
             Text = "Control Test";
             Load += MainForm_Load;
@@ -71,5 +116,9 @@
 
         private GroupBox grpFont;
         private Label IblFont;
+        private CheckBox chkIltalic;
+        private CheckBox chkBold;
+        private ComboBox cboFont;
+        private TextBox txtSampleText;
     }
 }
