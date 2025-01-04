45844e5019ad2d55c2f62613dd8ea57e4b3b73f7
vvvvo0 <idvvvvo@gmail.com>
2025-01-04 16:06:58 +0900

    컨트롤에 이벤트 처리기 등록(MainForm의 Load 이벤트에 대한 처리기 등록)

--- MainForm.cs
+++ MainForm.cs
@@ -12,9 +12,30 @@ namespace UsingControls
 
         }
 
+        // IDE가 MainForm_Load() 이벤트 처리기의 껍데기를 만들면서 코드 편집기를 자동으로 엽니다. 
         private void MainForm_Load(object sender, EventArgs e)
         {
+            var Fonts = FontFamily.Families; // 운영체제에 설치되어 있는 폰트 목록 검색
+            foreach(FontFamily font in Fonts) // cboFont 컨트롤에 각 폰트 이름 추가 
+                cboFont.Items.Add(font.Name);
+        }
 
+        // cboFont, chkBold, chkItalic 컨트롤의 이벤트 처리기에서 호출하기 위한 것으로,
+        // txtSampleText의 문자열 폰트를 변경하는 기능을 합니다.
+        void ChangeFont()
+        {
+            if (cboFont.SelectedIndex < 0)
+                return;
+
+            FontStyle style = FontStyle.Regular;
+            if (chkBold.Checked)
+                style |= FontStyle.Bold;
+            if (chkItalic.Checked)
+                style |= FontStyle.Italic;
+
+            txtSampleText.Font =
+                new Font((string)cboFont.SelectedItem, 10,
+                    style);
         }
     }
 }
--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # UsingControls
+
+컨트롤 배치:
+![스크린샷 2025-01-04 154935](https://github.com/user-attachments/assets/1438df0a-b52a-488a-843c-97739aef22ba)
