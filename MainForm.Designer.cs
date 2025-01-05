74f74d4343a916afd6f42deec41f26e9dcba01a2
vvvvo0 <idvvvvo@gmail.com>
2025-01-05 13:12:43 +0900

    ComboBox, CheckBox, TextBox 컨트롤에 대해 이벤트 처리기 껍데기를 만들고, 만들어 놓은 메서드를 호출

--- MainForm.Designer.cs
+++ MainForm.Designer.cs
@@ -29,45 +29,54 @@
         private void InitializeComponent()
         {
             grpFont = new GroupBox();
-            IblFont = new Label();
-            cboFont = new ComboBox();
-            chkBold = new CheckBox();
-            chkIltalic = new CheckBox();
             txtSampleText = new TextBox();
+            chkltalic = new CheckBox();
+            chkBold = new CheckBox();
+            cboFont = new ComboBox();
+            IblFont = new Label();
+            groupBox1 = new GroupBox();
+            grpBar = new GroupBox();
+            tbDummy = new TrackBar();
+            pgDummy = new ProgressBar();
             grpFont.SuspendLayout();
+            grpBar.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)tbDummy).BeginInit();
             SuspendLayout();
             // 
             // grpFont
             // 
+            grpFont.Controls.Add(groupBox1);
             grpFont.Controls.Add(txtSampleText);
-            grpFont.Controls.Add(chkIltalic);
+            grpFont.Controls.Add(chkltalic);
             grpFont.Controls.Add(chkBold);
             grpFont.Controls.Add(cboFont);
             grpFont.Controls.Add(IblFont);
             grpFont.Location = new Point(21, 35);
             grpFont.Name = "grpFont";
-            grpFont.Size = new Size(584, 360);
+            grpFont.Size = new Size(566, 157);
             grpFont.TabIndex = 0;
             grpFont.TabStop = false;
             grpFont.Text = "ComboBox, CheckBox, TextBox";
             grpFont.Enter += groupBox1_Enter;
             // 
-            // IblFont
+            // txtSampleText
             // 
-            IblFont.AutoSize = true;
-            IblFont.Location = new Point(43, 46);
-            IblFont.Name = "IblFont";
-            IblFont.Size = new Size(38, 15);
-            IblFont.TabIndex = 0;
-            IblFont.Text = "Font :";
+            txtSampleText.Location = new Point(44, 87);
+            txtSampleText.Name = "txtSampleText";
+            txtSampleText.Size = new Size(375, 23);
+            txtSampleText.TabIndex = 4;
+            txtSampleText.Text = "Hello, C#";
             // 
-            // cboFont
+            // chkltalic
             // 
-            cboFont.FormattingEnabled = true;
-            cboFont.Location = new Point(89, 43);
-            cboFont.Name = "cboFont";
-            cboFont.Size = new Size(198, 23);
-            cboFont.TabIndex = 1;
+            chkltalic.AutoSize = true;
+            chkltalic.Location = new Point(358, 45);
+            chkltalic.Name = "chkltalic";
+            chkltalic.Size = new Size(62, 19);
+            chkltalic.TabIndex = 3;
+            chkltalic.Text = "이탤릭";
+            chkltalic.UseVisualStyleBackColor = true;
+            chkltalic.CheckedChanged += chkItalic_CheckedChanged;
             // 
             // chkBold
             // 
@@ -78,30 +87,71 @@
             chkBold.TabIndex = 2;
             chkBold.Text = "굵게";
             chkBold.UseVisualStyleBackColor = true;
+            chkBold.CheckedChanged += chkBold_CheckedChanged;
             // 
-            // chkIltalic
+            // cboFont
             // 
-            chkIltalic.AutoSize = true;
-            chkIltalic.Location = new Point(358, 45);
-            chkIltalic.Name = "chkIltalic";
-            chkIltalic.Size = new Size(62, 19);
-            chkIltalic.TabIndex = 3;
-            chkIltalic.Text = "이탤릭";
-            chkIltalic.UseVisualStyleBackColor = true;
+            cboFont.FormattingEnabled = true;
+            cboFont.Location = new Point(89, 43);
+            cboFont.Name = "cboFont";
+            cboFont.Size = new Size(198, 23);
+            cboFont.TabIndex = 1;
+            cboFont.SelectedIndexChanged += cboFont_SelectedIndexChanged;
             // 
-            // txtSampleText
+            // IblFont
             // 
-            txtSampleText.Location = new Point(44, 87);
-            txtSampleText.Name = "txtSampleText";
-            txtSampleText.Size = new Size(375, 23);
-            txtSampleText.TabIndex = 4;
-            txtSampleText.Text = "Hello, C#";
+            IblFont.AutoSize = true;
+            IblFont.Location = new Point(43, 46);
+            IblFont.Name = "IblFont";
+            IblFont.Size = new Size(38, 15);
+            IblFont.TabIndex = 0;
+            IblFont.Text = "Font :";
+            // 
+            // groupBox1
+            // 
+            groupBox1.Location = new Point(31, 157);
+            groupBox1.Name = "groupBox1";
+            groupBox1.Size = new Size(528, 192);
+            groupBox1.TabIndex = 5;
+            groupBox1.TabStop = false;
+            groupBox1.Text = "groupBox1";
+            // 
+            // grpBar
+            // 
+            grpBar.Controls.Add(pgDummy);
+            grpBar.Controls.Add(tbDummy);
+            grpBar.Location = new Point(26, 236);
+            grpBar.Name = "grpBar";
+            grpBar.Size = new Size(567, 189);
+            grpBar.TabIndex = 1;
+            grpBar.TabStop = false;
+            grpBar.Text = "TrackBar && ProgressBar";
+            grpBar.Enter += chkItalic_CheckedChanged;
+            // 
+            // tbDummy
+            // 
+            tbDummy.Location = new Point(20, 37);
+            tbDummy.Maximum = 20;
+            tbDummy.Name = "tbDummy";
+            tbDummy.Size = new Size(528, 45);
+            tbDummy.TabIndex = 0;
+            tbDummy.Scroll += tbDummy_Scroll;
+            // 
+            // pgDummy
+            // 
+            pgDummy.Location = new Point(21, 109);
+            pgDummy.Maximum = 20;
+            pgDummy.Name = "pgDummy";
+            pgDummy.Size = new Size(529, 51);
+            pgDummy.TabIndex = 1;
+            pgDummy.Click += pgDummy_Click;
             // 
             // MainForm
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(621, 450);
+            Controls.Add(grpBar);
             Controls.Add(grpFont);
             FormBorderStyle = FormBorderStyle.FixedSingle;
             Name = "MainForm";
@@ -109,6 +159,9 @@
             Load += MainForm_Load;
             grpFont.ResumeLayout(false);
             grpFont.PerformLayout();
+            grpBar.ResumeLayout(false);
+            grpBar.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)tbDummy).EndInit();
             ResumeLayout(false);
         }
 
@@ -116,9 +169,13 @@
 
         private GroupBox grpFont;
         private Label IblFont;
-        private CheckBox chkIltalic;
+        private CheckBox chkltalic;
         private CheckBox chkBold;
         private ComboBox cboFont;
         private TextBox txtSampleText;
+        private GroupBox groupBox1;
+        private GroupBox grpBar;
+        private ProgressBar pgDummy;
+        private TrackBar tbDummy;
     }
 }
--- MainForm.cs
+++ MainForm.cs
@@ -16,13 +16,14 @@ namespace UsingControls
         private void MainForm_Load(object sender, EventArgs e)
         {
             var Fonts = FontFamily.Families; // 운영체제에 설치되어 있는 폰트 목록 검색
-            foreach(FontFamily font in Fonts) // cboFont 컨트롤에 각 폰트 이름 추가 
+            foreach (FontFamily font in Fonts) // cboFont 컨트롤에 각 폰트 이름 추가 
                 cboFont.Items.Add(font.Name);
         }
 
+
         // cboFont, chkBold, chkItalic 컨트롤의 이벤트 처리기에서 호출하기 위한 것으로,
         // txtSampleText의 문자열 폰트를 변경하는 기능을 합니다.
-        void ChangeFont() 
+        void ChangeFont()
         {
             if (cboFont.SelectedIndex < 0) // cboFont에서 선택한 항목이 없으면 메서드 종료
                 return;
@@ -32,11 +33,30 @@ namespace UsingControls
             if (chkBold.Checked) // 만약 "굵게" 체크 박스가 선택되어 있으면 Bold 논리합 수행함
                 style |= FontStyle.Bold;
 
-            if (chkItalic.Checked) // 만약 "이탤릭" 체크 박스가 선택되어 있으면 Italic 논리합 수행함
+            if (chkltalic.Checked) // 만약 "이탤릭" 체크 박스가 선택되어 있으면 Italic 논리합 수행함
                 style |= FontStyle.Italic;
 
             txtSampleText.Font =     // txtSampleText의 Font 프로퍼티를 앞에서 만든 style로 수정함
                 new Font((string)cboFont.SelectedItem, 10, style);
+        }
+
+
+        // 각 컨트롤에 대해 이벤트 처리기 껍데기를 만듭니다.
+        // (MainForm.cs [디자인] 탭에서 각 컨트롤을 더블 클릭하면 생성됩니다.)
+        // 그리고 각 이벤트 처리기에 조금 전에 선언한 ChangeFont() 메서드를 호출합니다.
+        private void cboFont_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            ChangeFont();
+        }
+
+        private void chkBold_CheckedChanged(object sender, EventArgs e)
+        {
+            ChangeFont();
+        }
+
+        private void chkItalic_CheckedChanged(object sender, EventArgs e)
+        {
+            ChangeFont();
         }
     }
 }
